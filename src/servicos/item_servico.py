12c248bf9f21e9f56ffce0b67a6f8e7060ccd82f
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2024-03-22 22:11:25 -0300

    Alterado boa parte dos serviços

--- src/servicos/item_servico.py
+++ src/servicos/item_servico.py
@@ -1,47 +1,43 @@
-import random, pygame, time
+import random
 
-from entities.Item import Item
+from entities.Espatula import Espatula
+from entities.Pano import Pano
+from entities.Pizza import Pizza
 from entities.Personagem import Personagem
 
 class Item_Servico:
-    
-    def criar_item():
-        itemCriado = Item(0, 300, 40, 40, random.uniform(5, 10), random.uniform(-5, -15))  # Velocidade horizontal inicial aleatória)
-        return itemCriado
-    
-    def desenhar_item(itemDesenho, screen, imagem):
-        #imagem_pizza = pygame.image.load("assets/Imagens/pizza.png")
-        
-        # Verifica se o objeto é do tipo Item
-        if isinstance(itemDesenho, Item):
-            itemDesenho.desenhar(screen, imagem)
 
-    def movimento_item(itemMovimenta, gravidade):
+    def __init__(self):
+        self.classes_item = [Pizza, Pano, Espatula]
+    
+    def criar_item(self):
+        classe_item = random.choice(self.classes_item)
+        itemCriado = classe_item(0, 300, 40, 40, random.uniform(5, 10), random.uniform(-5, -15))  # Velocidade horizontal inicial aleatória)
+        return itemCriado
+
+    def desenhar_item(self, itemDesenho, screen):        
+        if any(isinstance(itemDesenho, classe) for classe in self.classes_item):
+            itemDesenho.desenhar(screen)
+
+    def movimento_item(self, itemMovimenta, gravidade):
         # Verifica se o objeto é do tipo Item
-        if isinstance(itemMovimenta, Item):
+        if any(isinstance(itemMovimenta, classe) for classe in self.classes_item):
             itemMovimenta.movimento_parabolico(gravidade)
 
-    def reinicia_item(itemReinicia):
+    def reinicia_item_sumiu(self, itemReinicia):
         # Verifica se o objeto é do tipo Item
-        if isinstance(itemReinicia, Item):
+        if any(isinstance(itemReinicia, classe) for classe in self.classes_item):
             if itemReinicia.posicao.y > 600:
-                itemReinicia.reinicia_item()
+                itemReinicia.reinicia()
 
-    def checa_colisao(personagem, itemColide):
+    def reinicia_item_coletou(self, itemReinicia):
+        if any(isinstance(itemReinicia, classe) for classe in self.classes_item):
+            itemReinicia.reinicia()
+
+    def checa_colisao(self, personagem, itemColide):
         if isinstance(personagem, Personagem):
-            if isinstance(itemColide, Item):
+            if any(isinstance(itemColide, classe) for classe in self.classes_item):
                 return(personagem.posicao.x < itemColide.posicao.x + itemColide.largura and 
                     personagem.posicao.x + personagem.altura > itemColide.posicao.x and 
                     personagem.posicao.y < itemColide.posicao.y + itemColide.largura and 
                     personagem.posicao.y + personagem.altura > itemColide.posicao.y)
-            
-    def definir_imagem(id):
-        if (id == 0):
-            imagem_pizza = pygame.image.load("assets/Imagens/pizza.png")
-            return imagem_pizza
-        if (id == 1):
-            imagem_espatula = pygame.image.load("assets/Imagens/Espátula 300.png")
-            return imagem_espatula
-        if (id == 2):
-            imagem_pano = pygame.image.load("assets/Imagens/Pano 300.png")
-            return imagem_pano
--- src/servicos/personagem_servico.py
+++ src/servicos/personagem_servico.py
@@ -2,22 +2,26 @@ from entities.Personagem import Personagem
 
 class Personagem_Servico:
 
-    def criar_personagem(tela_largura, tela_altura, posicao_x_ratio, posicao_y_ratio):
+    def criar_personagem(self, tela_largura, tela_altura, posicao_x_ratio, posicao_y_ratio):
         personagemCriado = Personagem((237, 14, 178), int(posicao_x_ratio * tela_altura), int(posicao_y_ratio * tela_largura), 40, 40, 5)
         return personagemCriado
     
-    def desenhar_personagem(personagemDesenho, tela):
+    def desenhar_personagem(self, personagemDesenho, tela):
         if isinstance(personagemDesenho, Personagem):
             personagemDesenho.desenhar(tela)
 
-    def andar_esquerda(self):
-        Personagem.andar_esquerda()
+    def andar_esquerda(self, personagem):
+        if isinstance(personagem, Personagem): 
+            personagem.andar_esquerda()
     
-    def andar_direita(self):
-        Personagem.andar_direita()
+    def andar_direita(self, personagem, tela_altura):
+        if isinstance(personagem, Personagem):
+            personagem.andar_direita(tela_altura)
     
-    def coletar_item(self):
-        Personagem.coletar_item()
+    def coletar_item(self, personagem):
+        if isinstance(personagem, Personagem):
+            personagem.coletar_item()
 
-    def pegar_itens_coletados(self):
-        return Personagem.pegar_itens_coletados()
+    def pegar_itens_coletados(self, personagem):
+        if isinstance(personagem, Personagem):
+            return personagem.pegar_itens_coletados()
