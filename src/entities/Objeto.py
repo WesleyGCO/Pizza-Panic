95ed30bd77990bf19cbaf0204a56c272e9f1b57b
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2024-07-10 20:06:21 -0300

    Correção na classe Objeto

--- src/entities/Objeto.py
+++ src/entities/Objeto.py
@@ -2,13 +2,12 @@ from entities.Vetor import Vetor
 import random
 
 class Objeto:
-    def __init__(self, x, y, largura, altura, velocidade):
+    def __init__(self, x, y, largura, altura):
         self.posicao = Vetor(x, y)
         self.largura = largura
         self.altura = altura
         self.start_x = self.posicao.x
         self.start_y = self.posicao.y
-        self.velocidade = velocidade
 
     def desenhar(self, screen):
         pass
